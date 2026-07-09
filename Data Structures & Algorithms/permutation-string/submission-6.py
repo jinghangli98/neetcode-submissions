18ea114d60d571be042270dc044d1573d4326cd7
Jinghang Li <64978031+jinghangli98@users.noreply.github.com>
2026-07-09 19:29:14 -0400

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+
+        s1_table = [0] * 26
+        s2_table = [0] * 26
+
+        for i in range(len(s1)):
+            s1_table[ord(s1[i]) - ord('a')] += 1
+            s2_table[ord(s2[i]) - ord('a')] += 1
+        
+        if s1_table == s2_table:
+            return True
+        
+        for right in range(len(s1), len(s2)):
+            s2_table[ord(s2[right]) - ord('a')] += 1
+            
+            left = right - len(s1)
+            s2_table[ord(s2[left]) - ord('a')] -= 1
+        
+            if s1_table == s2_table:
+                return True
+        
+        return False
